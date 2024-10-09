4d9908bd3f2097caf37d727a9f6177e288d29dde
FatemaPromi <siddiqua.promi@gmail.com>
2024-10-09 17:21:43 -0600

    Create Data Combining.sql

---
+++ Data Combining.sql
@@ -0,0 +1,32 @@
+-- Combining all 12 months of data in one table.
+
+CREATE TABLE `case-study-1-435815.bike_trips.combined_year` AS (
+  SELECT * FROM `bike_trips.202201_tripdata`
+  UNION ALL
+  SELECT * FROM `bike_trips.202202_tripdata`
+  UNION ALL
+  SELECT * FROM `bike_trips.202203_tripdata`
+  UNION ALL
+  SELECT * FROM `bike_trips.202204_tripdata`
+  UNION ALL
+  SELECT * FROM `bike_trips.202205_tripdata`
+  UNION ALL
+  SELECT * FROM `bike_trips.202206_tripdata`
+  UNION ALL
+  SELECT * FROM `bike_trips.202207_tripdata`
+  UNION ALL
+  SELECT * FROM `bike_trips.202208_tripdata`
+  UNION ALL
+  SELECT * FROM `bike_trips.202209_tripdata`
+  UNION ALL
+  SELECT * FROM `bike_trips.202210_tripdata`
+  UNION ALL
+  SELECT * FROM `bike_trips.202211_tripdata`
+  UNION ALL
+  SELECT * FROM `bike_trips.202212_tripdata`
+);
+
+-- checking no of rows which are 5667717
+
+SELECT COUNT(*)
+FROM `case-study-1-435815.bike_trips.combined_year`;
